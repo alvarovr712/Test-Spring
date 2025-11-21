54a46656fd5b86ad6a59e58c4917928500c6bf78
Alvaro <alvarovr712@gmail.com>
2025-11-21 20:16:09 +0100

    Proyecto inicial

---
+++ src/test/java/com/example/Test/Spring/TestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Test.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
